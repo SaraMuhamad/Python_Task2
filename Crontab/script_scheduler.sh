96f913759f5ab30a6b627fb87132a1134a9a625c
SaraMuhamad <75182294+SaraMuhamad@users.noreply.github.com>
2021-04-23 10:44:13 +0200

    Add files via upload

---
+++ Crontab/script_scheduler.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cd /home/sara/Desktop/ITI_Python_for_Data_Management/Task2/MyScript
+
+/home/sara/anaconda3/bin/python3 script.py /home/sara/Desktop/ITI_Python_for_Data_Management/Task2/sample_data_for_testing > execution_logger.log
